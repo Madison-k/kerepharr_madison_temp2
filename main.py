2adc4155424172c15dbb2f1770d25612dca4289f
Madison-k <72760208+Madison-k@users.noreply.github.com>
2020-11-20 12:27:59 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ while True:
         light.set_pixel_color(5, light.rgb(255, 0, 0))
     else :
          light.clear()
-         
+
+#if tempF > 60:
+    #light.set_pixel_color(5, light.rbg(0, 255, 0))
+
+if tempf >1000:
+    light.set_pixel_color(5, light.rgb(255, 0, 0))
+elif tempf > 40:
+    light.set_pixel_color(5, light.rgb(0, 255, 0))
+else:
+    light.set_pixel_color(5, light.rgb (0, 0, 255))
+
+#if tempF > 70:
+    #light.set_all(light.rbg(255, 0, 0))
+#elif tempF > 40:
+    #light.set_all(lighrt.rbg(0, 255, 0)) 
+#else:
+    #light.set_all(light.rbg(0, 0, 255))       
--- main.ts
+++ main.ts
@@ -8,3 +8,13 @@ while (true) {
     }
     
 }
+// if tempF > 60:
+// light.set_pixel_color(5, light.rbg(0, 255, 0))
+if (tempf > 1000) {
+    light.setPixelColor(5, light.rgb(255, 0, 0))
+} else if (tempf > 40) {
+    light.setPixelColor(5, light.rgb(0, 255, 0))
+} else {
+    light.setPixelColor(5, light.rgb(0, 0, 255))
+}
+
